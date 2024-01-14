313f0c97c47d1c5c606effb085a4324c22e61186
Lance Desk <lncedesk@gmail.com>
2024-01-14 01:13:23 -0800

    A script that prepares a MySQL server for the project.

---
+++ setup_mysql_test.sql
@@ -0,0 +1,15 @@
+-- setup_mysql_test.sql
+
+-- Create or use the database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create or use the user hbnb_test in localhost with password hbnb_test_pwd
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
