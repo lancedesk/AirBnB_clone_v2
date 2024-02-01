1c08d305f758c012dc894c9c48e5c94b5249c9ab
Lance Desk <lncedesk@gmail.com>
2024-02-01 06:48:34 -0800

    A Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy:

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,30 +3,25 @@
 Fabric script that creates and distributes an archive to my two web servers
 """
 
-from fabric.api import env, local, put, run
-from os.path import exists
-from datetime import datetime
+from fabric.api import env, put, run, local
+import os.path
+from time import strftime
 
-env.user = "ubuntu"
-env.hosts = ["54.90.14.221", "204.236.240.155"]
+env.hosts = ['54.90.14.221', '204.236.240.155']
 
 
 def do_pack():
     """
-    Creates a compressed archive of the web_static directory
+    Creates a compressed archive of the web_static directory.
 
     Returns:
         str: Path to the created archive if successful, None otherwise.
     """
     try:
-        if not exists("versions"):
-            local("mkdir -p versions")
-
-        date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_name = "versions/web_static_{}.tgz".format(date_time)
-
-        local("tar -czvf {} web_static".format(file_name))
-
+        date_time = strftime('%Y%M%d%H%M%S')
+        local('mkdir -p versions')
+        file_name = 'versions/web_static_{}.tgz'.format(date_time)
+        local('tar -czvf {} web_static/'.format(file_name))
         return file_name
     except Exception:
         return None
@@ -34,7 +29,7 @@ def do_pack():
 
 def do_deploy(archive_path):
     """
-    Deploys the compressed archive to the web servers
+    Deploys the compressed archive to the web servers.
 
     Args:
         archive_path (str): Path to the archive file to deploy.
@@ -42,27 +37,21 @@ def do_deploy(archive_path):
     Returns:
         bool: True if deployment is successful, False otherwise.
     """
-    if not exists(archive_path):
+    if not os.path.isfile(archive_path):
         return False
-
     try:
-        file_name = archive_path.split("/")[-1]
-        file_name_no_ext = file_name.split(".")[0]
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(file_name_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, file_name_no_ext))
-        run("rm /tmp/{}".format(file_name))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/"
-            .format(file_name_no_ext, file_name_no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(file_name_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current"
-            .format(file_name_no_ext))
+        file_name = archive_path.split('/')[-1]
+        no_ext = file_name.split('.')[0]
+        path_no_ext = '/data/web_static/releases/{}/'.format(no_ext)
+        symbolic_link = '/data/web_static/current'
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(path_no_ext))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, path_no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}web_static/* {}'.format(path_no_ext, path_no_ext))
+        run('rm -rf {}web_static'.format(path_no_ext))
+        run('rm -rf {}'.format(symbolic_link))
+        run('ln -s {} {}'.format(path_no_ext, symbolic_link))
         return True
     except Exception:
         return False
@@ -70,7 +59,7 @@ def do_deploy(archive_path):
 
 def deploy():
     """
-    Packs the web_static folder and deploys it to the web servers
+    Packs the web_static folder and deploys it to the web servers.
 
     Returns:
         bool: True if deployment is successful, False otherwise.
@@ -78,5 +67,5 @@ def deploy():
     archive_path = do_pack()
     if archive_path is None:
         return False
-
-    return do_deploy(archive_path)
+    deploy = do_deploy(archive_path)
+    return deploy
