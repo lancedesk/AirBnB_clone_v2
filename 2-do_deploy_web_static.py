fd7e88d635004d21182fa368d61d66facf063ccb
Lance Desk <lncedesk@gmail.com>
2024-02-01 05:00:54 -0800

    A Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,17 +4,15 @@ Fabric script that distributes an archive to your web servers
 """
 
 from fabric.api import env, put, run
-import os
+import os.path
 
-# Define your servers' IP addresses
+# Servers' IP addresses
 env.hosts = ['54.90.14.221', '204.236.240.155']
-# Define the user to connect as
-env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """
-    Distributes an archive to your web servers
+    Distributes an archive to the two web servers
 
     Args:
         archive_path (str): Path to the archive file to deploy.
@@ -22,35 +20,33 @@ def do_deploy(archive_path):
     Returns:
         bool: True if the deployment is successful, False otherwise.
     """
-    if not os.path.exists(archive_path):
-        print(f"Error: Archive file '{archive_path}' does not exist.")
+
+    if not os.path.isfile(archive_path):
         return False
-
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/")
-
-        # Extract the archive to the releases directory
-        archive_filename = os.path.basename(archive_path)
+        archive_filename = archive_path.split('/')[-1]
         archive_name_no_ext = archive_filename.split('.')[0]
-        releases_directory = "/data/web_static/releases/"
-        release_folder = releases_directory + archive_name_no_ext + "/"
-        run("mkdir -p {}".format(release_folder))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
+        releases_dir = "/data/web_static/releases"
+        path_no_ext = '{}/{}/'.format(releases_dir, archive_name_no_ext)
+        symbolic_link = '/data/web_static/current'
+
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(path_no_ext))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, path_no_ext))
 
         # Delete the uploaded archive from the web server
-        run("rm /tmp/{}".format(archive_filename))
+        run('rm /tmp/{}'.format(archive_filename))
 
         # Move contents of release_folder/web_static/ to release_folder/
-        run("mv {}web_static/* {}".format(release_folder, release_folder))
-        run("rm -rf {}web_static".format(release_folder))
+        run('mv {}web_static/* {}'.format(path_no_ext, path_no_ext))
 
         # Update the symbolic link /data/web_static/current
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(release_folder))
+        run('rm -rf {}web_static'.format(path_no_ext))
+        run('rm -rf {}'.format(symbolic_link))
+        run('ln -s {} {}'.format(path_no_ext, symbolic_link))
 
-        print("New version deployed successfully!")
         return True
-    except Exception as e:
-        print(f"Error: Deployment failed - {e}")
+
+    except Exception:
         return False
