1d63491abbeccf085afa4bf6449959b5dcab0418
Lance Desk <lncedesk@gmail.com>
2024-02-01 04:34:28 -0800

    A Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers
+"""
+
+from fabric.api import env, put, run
+import os
+
+# Define your servers' IP addresses
+env.hosts = ['IP_web-01', 'IP_web-02']
+# Define the user to connect as
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to your web servers
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        # folder = "/data/web_static/releases/<archive"
+        # Uncompress archive to the folder {folder} filename without extension>
+        archive_filename = os.path.basename(archive_path)
+        archive_name_no_ext = archive_filename.split('.')[0]
+        releases_directory = "/data/web_static/releases/"
+        release_folder = releases_directory + archive_name_no_ext + "/"
+        run("mkdir -p {}".format(release_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
+
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(archive_filename))
+
+        # Move contents of release_folder/web_static/ to release_folder/
+        run("mv {}web_static/* {}".format(release_folder, release_folder))
+        run("rm -rf {}web_static".format(release_folder))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run("ln -s {} /data/web_static/current".format(release_folder))
+
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        return False
