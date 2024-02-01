dd0531edd1d1839c7cc97741befc71c4bc90707b
Lance Desk <lncedesk@gmail.com>
2024-02-01 05:49:22 -0800

    A Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,43 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Fabric script that distributes an archive to my two web servers
 """
 
 import os
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+from fabric.api import *
 
-env.hosts = ['54.90.14.221', '204.236.240.155']
+env.user = "ubuntu"
+env.hosts = ["54.90.14.221", "204.236.240.155"]
 
 
-def do_deploy(archive_path):
+def do_pack():
     """
-    Creates a compressed archive of the web_static directory,
-    packs it with a timestamp, and stores it in the versions directory.
-
-    Args:
-        archive_path (str): The path to the archive file.
+    Creates a compressed archive of the web_static directory
+    and stores it in the versions directory.
 
     Returns:
-        str: The path to created archive file if successful, None otherwise.
+        str: Path to created archive file if successful, None otherwise.
     """
-
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    current_time = datetime.now()
-
-    zip_output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        current_time.year,
-        current_time.month,
-        current_time.day,
-        current_time.hour,
-        current_time.minute,
-        current_time.second
-    )
-
     try:
-        print("Packing web_static to {}".format(zip_output))
+        if not os.path.isdir("versions"):
+            os.makedirs("versions")
+
+        date = datetime.now()
+        zip_output = "versions/web_static_{0}{1}{2}{3}{4}{5}".format(
+            date.year,
+            date.month,
+            date.day,
+            date.hour,
+            date.minute,
+            date.second
+        )
+        zip_output += ".tgz"
         local("tar -cvzf {} web_static".format(zip_output))
-        zip_size = os.stat(zip_output).st_size
-        print("web_static packed: {} -> {} Bytes".format(zip_output, zip_size))
+        return zip_output
 
     except Exception:
-        zip_output = None
-
-    return zip_output
+        return None
 
 
 def do_deploy(archive_path):
@@ -52,33 +45,46 @@ def do_deploy(archive_path):
     Deploys the compressed archive to the web servers.
 
     Args:
-        archive_path (str): The path to the archive file.
+        archive_path (str): Path to the archive file to deploy.
 
     Returns:
         bool: True if the deployment is successful, False otherwise.
     """
 
-    if not os.path.exists(archive_path):
-        return False
-
-    zip_name = os.path.basename(archive_path)
-    dir_name = zip_name.replace(".tgz", "")
-    dir_path = "/data/web_static/releases/{}/".format(dir_name)
-
-    success = False
     try:
-        put(archive_path, "/tmp/{}".format(zip_name))
-        run("mkdir -p {}".format(dir_path))
-        run("tar -xzf /tmp/{} -C {}".format(zip_name, dir_path))
-        run("rm -rf /tmp/{}".format(zip_name))
-        run("mv {}web_static/* {}".format(dir_path, dir_path))
-        run("rm -rf {}web_static".format(dir_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(dir_path))
-        print('New version deployed!')
-        success = True
+        if not os.path.isfile(archive_path):
+            return False
+
+        zip_path = archive_path.split("/")[1]
+        zip_name = zip_path.split(".")[0]
+        put(archive_path, "/tmp/{0}".format(zip_path))
+
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(zip_name))
+        src = "sudo tar -xzf /tmp/{0} -C".format(zip_path)
+        dst = "/data/web_static/releases/{0}/".format(zip_name)
+
+        run(src + " " + dst)
+        run("sudo rm /tmp/{0}".format(zip_path))
+
+        releases_dir = "/data/web_static/releases"
+        src = (
+            "sudo mv {}/{}/web_static/*".format(releases_dir, zip_name)
+        )
+
+        dst = "/data/web_static/releases/{0}/".format(zip_name)
+        run(src + " " + dst)
+
+        releases_2_dir = "/data/web_static/releases"
+        run(
+            "sudo rm -rf {}/{}/web_static".format(releases_2_dir, zip_name)
+        )
+
+        run("sudo rm -rf /data/web_static/current")
+        src = "sudo ln -s /data/web_static/releases/{0}/".format(zip_name)
+        dst = "/data/web_static/current"
+        run(src + " " + dst)
+
+        return True
 
     except Exception:
-        success = False
-
-    return success
+        return False
