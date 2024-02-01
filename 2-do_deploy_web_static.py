7b4b81a9e9523615daefa6f857893101d6750717
Lance Desk <lncedesk@gmail.com>
2024-02-01 04:41:56 -0800

    A Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import env, put, run
 import os
 
 # Define your servers' IP addresses
-env.hosts = ['IP_web-01', 'IP_web-02']
+env.hosts = ['54.90.14.221', '204.236.240.155']
 # Define the user to connect as
 env.user = 'ubuntu'
 
@@ -15,15 +15,22 @@ env.user = 'ubuntu'
 def do_deploy(archive_path):
     """
     Distributes an archive to your web servers
+
+    Args:
+        archive_path (str): Path to the archive file to deploy.
+
+    Returns:
+        bool: True if the deployment is successful, False otherwise.
     """
     if not os.path.exists(archive_path):
+        print(f"Error: Archive file '{archive_path}' does not exist.")
         return False
 
     try:
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-        # folder = "/data/web_static/releases/<archive"
-        # Uncompress archive to the folder {folder} filename without extension>
+
+        # Extract the archive to the releases directory
         archive_filename = os.path.basename(archive_path)
         archive_name_no_ext = archive_filename.split('.')[0]
         releases_directory = "/data/web_static/releases/"
@@ -31,20 +38,19 @@ def do_deploy(archive_path):
         run("mkdir -p {}".format(release_folder))
         run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_folder))
 
-        # Delete the archive from the web server
+        # Delete the uploaded archive from the web server
         run("rm /tmp/{}".format(archive_filename))
 
         # Move contents of release_folder/web_static/ to release_folder/
         run("mv {}web_static/* {}".format(release_folder, release_folder))
         run("rm -rf {}web_static".format(release_folder))
 
-        # Delete the symbolic link /data/web_static/current from the web server
+        # Update the symbolic link /data/web_static/current
         run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link
         run("ln -s {} /data/web_static/current".format(release_folder))
 
-        print("New version deployed!")
+        print("New version deployed successfully!")
         return True
     except Exception as e:
+        print(f"Error: Deployment failed - {e}")
         return False
