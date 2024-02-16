b633c5f20d5e40c383d3941b061cc34a69eea094
Lance Desk <lncedesk@gmail.com>
2024-02-16 03:50:04 -0800

    A script that starts a Flask web application.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,30 +3,29 @@
 Starts a Flask web application that listens on 0.0.0.0, port 5000
 """
 
-from flask import Flask, render_template
 from models import storage
-from models.state import State
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     """
     Displays a HTML page with a list of states
     """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(exc):
     """
     Removes the current SQLAlchemy Session
     """
     storage.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
